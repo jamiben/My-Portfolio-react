1bf6c19817cee6e5f4d32cb360e0c9201ed2671c
jamiben <jamilazerara@yahoo.fr>
2023-03-08 18:44:57 +0100

    app modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MyContext from './Context/Context'
 
-import Header from '../src/Components/Header/Header'
-import Profil from '../src/Components/Profil/Profil';
+import CanvasComponent from './Components/Canva/Canva';
+import Header from './Components/Canva/Header/Header'
+import Profil from './Components/Canva/Profil/Profil';
 // import Contact from './Components/Contact/Contact'
 import './App.css';
 import Error from './Components/Error/Error';
-import Projects from './Components/Projects/Projects';
+import Projects from './Context/Projects/Projects';
+import ParentNode from './Context/Context';
 
 function App() {
+
+  const [burgerButtonOpen, setBurgerButtonOpen] = useState(false);
+
+
   return (
-      <div className='appContent'>
-        <Header />
+    <MyContext.Provider value={{burgerButtonOpen, setBurgerButtonOpen}}>
+
+      <div className='contentProfile'>
+
+        <CanvasComponent />
         <BrowserRouter>  
           <Routes>
-            <Route path="*" element={< Error/>}/>
-            <Route index element={<Profil />} />
+            {/* <Route path="*" element={< Error/>}/> */}
+            <Route path="/profil" element={<Profil />} />
             <Route path="/projects" element={<Projects />} />
             {/* <Route path="/contact" element={<Contact />}/> */}
           </Routes>
         </BrowserRouter>
       </div>
+    </MyContext.Provider>
 
 
   );
