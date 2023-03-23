b96f97cc1fd332e27fd33863c464b00f110271fd
jamiben <jamilazerara@yahoo.fr>
2023-03-23 01:08:31 +0100

    contact and form

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ContactForm from './ContactForm/contactForm'
+import './Contact.scss';
+
 
 export default function Contact() {
 
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="contentContact">
+            <h1 className="titleContact">Contact</h1>
+            <div className="contentTxtForm">
+                <div className="contentPara">
+                    <p className="paraContatc">Site web</p>
+                    <p className="paraContatc">Site vitrine</p>
+                    <p className="paraContatc">Refonte </p>
+                    <p className="paraContatc">Référencement</p>
+                    <p className="paraContatc">N'hesitez pas à me contacter pour un devis et une éventuel collaboration</p>
+                    <p className="paraContatc">0635395040</p>
+                </div>
+                <ContactForm />
+            </div>
         </div>
         );
 }
--- src/Components/Contact/ContactForm/contactForm.tsx
+++ src/Components/Contact/ContactForm/contactForm.tsx
@@ -1,1 +1,20 @@
 import React from 'react';
+import './contactForm.scss'
+
+export default function ContatcForm() {
+
+    return(
+        <div className="contentForm">
+            <input type="text" placeholder='Name' className="step1 " />
+            <input type="text" placeholder='Socity' className="step2 inp" />
+            <select name="" id="" className="step3 inp">
+                <option value="" className="options">Concéption site administration</option>
+                <option value="" className="options">Conception Site vitrine</option>
+                <option value="" className="options">Mission Freelance</option>
+                <option value="" className="options">Autres</option>
+            </select>
+            <input type="text" placeholder='Email' className="step4 inp" />
+            <input type="text" placeholder='Phone number' className="step5 inp" />
+        </div>
+    )
+}
